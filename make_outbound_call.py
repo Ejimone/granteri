cbff81d00c574d76a7ab4b3be2c1437663568858
Evidence Ejimone <23btrcn075@jainuniversity.ac.in>
2025-06-04 22:18:18 +0530

    Remove commented-out call to make_outbound_call with phone_number_id

--- make_outbound_call.py
+++ make_outbound_call.py
@@ -27,4 +27,3 @@ def make_outbound_call(assistant_id: str, phone_number: str):
 
 #  number for testing
 make_outbound_call(assistant_id, "+917204218098")
-# make_outbound_call(assistant_id, phone_number_id)
