1d54362e03e3b5047ba2cef551abf5345ced3f55
Evidence Ejimone <23btrcn075@jainuniversity.ac.in>
2025-06-05 12:43:10 +0530

    Add imports for Vapi, dotenv, and support assistant in agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,6 @@
 # the agent will be running, make it will be able to make calls and recieve calls
+from vapi import Vapi
+import os
+from dotenv import load_dotenv
+from support_assistant import create_support_assistant
+from inbound_calls import configure_inbound_calls
